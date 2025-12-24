915782ffcef1bb16cef8d74be01fffd578c32bc7
Collins Kiaritha <thuocollins020@gmail.com>
2025-12-24 13:28:55 +0300

    added all database migrations

--- server/testing/conftest.py
+++ server/testing/conftest.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python3
 
+import pytest
+from app import app, db
+from models import Plant
+
+@pytest.fixture(scope="session", autouse=True)
+def seed_database():
+    with app.app_context():
+        db.create_all()
+        Plant.query.delete()
+        aloe = Plant(id=1, name="Aloe", image="./images/aloe.jpg", price=11.50)
+        zz_plant = Plant(id=2, name="ZZ Plant", image="./images/zz-plant.jpg", price=25.98)
+        db.session.add_all([aloe, zz_plant])
+        db.session.commit()
+
 def pytest_itemcollected(item):
     par = item.parent.obj
     node = item.obj
